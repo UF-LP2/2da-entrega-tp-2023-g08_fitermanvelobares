2bd4443e73c3cb7a7ba2a064189aacb14ea596a9
SFiterman <ing.solfiterman@gmail.com>
2023-10-21 21:09:45 -0300

    Comentarios en los tests

--- test/test_Enfermero.py
+++ test/test_Enfermero.py
@@ -7,15 +7,18 @@ def test_PacienteRojo():
     Raul = Paciente("36421479","Raul","Perez",["No Respira"])
     Jose = Paciente("46789915","Jose","Swaig",["Otalgias", "Politraumatismo Grave"])
     Enfermero1 = Enfermero("57531189", "Silvio", "Mendex")
+    #creo pacientes y enfermero
     
     Enfermero1. Asignar_Color_Paciente(Raul)
     Enfermero1. Asignar_Color_Paciente(Jose)
+    #enfermero asigna colores a los pacientes
     
     assert Raul.Color == "Rojo"
     assert not Raul.Color == "Azul"
     
     assert Jose.Color == "Rojo"
     assert not Jose.Color == "Verde"
+    #chequeo colores
     
 
 def test_PacienteNaranja():
@@ -23,44 +26,69 @@ def test_PacienteNaranja():
     Raul = Paciente("36421479","Raul","Perez",["Coma"])
     Jose = Paciente("46789915","Jose","Swaig",["Otalgias", "Convulsiones"])
     Enfermero1 = Enfermero("57531189", "Silvio", "Mendex")
-    
+    #creo pacientes y enfermero
+     
     Enfermero1. Asignar_Color_Paciente(Raul)
     Enfermero1. Asignar_Color_Paciente(Jose)
-    
+    #enfermero asigna colores a los pacientes
+
     assert Raul.Color == "Naranja"
     assert not Raul.Color == "Azul"
     
     assert Jose.Color == "Naranja"
     assert not Jose.Color == "Verde"
-
+    #chequeo colores
 
 def test_PacienteAmarillo() -> None:
    
     Raul = Paciente("36421479","Raul","Perez",["Cefalea Brusca"])
     Jose = Paciente("46789915","Jose","Swaig",["Otalgias", "Hipertension Arterial"])
     Enfermero1 = Enfermero("57531189", "Silvio", "Mendex")
-    
+    #creo pacientes y enfermero
+     
     Enfermero1. Asignar_Color_Paciente(Raul)
     Enfermero1. Asignar_Color_Paciente(Jose)
+    #enfermero asigna colores a los pacientes
     
     assert Raul.Color == "Amarillo"
     assert not Raul.Color == "Naranja"
     
     assert Jose.Color == "Amarillo"
     assert not Jose.Color == "Verde"
-    
+    #chequeo colores
     
 def test_PacienteVerde():
    
     Raul = Paciente("36421479","Raul","Perez",["Otalgias"])
     Jose = Paciente("46789915","Jose","Swaig",["Odontalgias", "Traumatismos"])
     Enfermero1 = Enfermero("57531189", "Silvio", "Mendex")
-    
+    #creo pacientes y enfermero
+     
     Enfermero1. Asignar_Color_Paciente(Raul)
     Enfermero1. Asignar_Color_Paciente(Jose)
+    #enfermero asigna colores a los pacientes
     
     assert Raul.Color == "Verde"
     assert not Raul.Color == "Azul"
     
     assert Jose.Color == "Verde"
     assert not Jose.Color == "Rojo"
+    #chequeo colores
+    
+def test_PacienteAzul():
+   
+    Raul = Paciente("36421479","Raul","Perez",["Nada"])
+    Jose = Paciente("46789915","Jose","Swaig",["Resfrio", "Comezon en el brazo"])
+    Enfermero1 = Enfermero("57531189", "Silvio", "Mendex")
+    #creo pacientes y enfermero
+     
+    Enfermero1. Asignar_Color_Paciente(Raul)
+    Enfermero1. Asignar_Color_Paciente(Jose)
+    #enfermero asigna colores a los pacientes
+  
+    assert Raul.Color == "Azul"
+    assert not Raul.Color == "Verde"
+    
+    assert Jose.Color == "Azul"
+    assert not Jose.Color == "Rojo"
+    #chequeo colores
--- test/test_Medico.py
+++ test/test_Medico.py
@@ -8,25 +8,32 @@ import pytest
 def test_Atender_Paciente():
     NoAtendidos : List[Paciente] = []
     Medico1 = Medico("35876690", "Marcelo", "Cohen")
-
-    #no me importa el orden de la fila en este test, solo me importa que lo atienda
+    #creo Medico y la lista
+    
     Raul = Paciente("36421479","Raul","Perez",["Otalgias"])
     Jose = Paciente("46789915","Jose","Swaig",["Odontalgias", "Traumatismos"])
     Sofia = Paciente("54782464","Sofia Ana","Fleishman",["Esguinces"])
+    #creo pacientes
     
     NoAtendidos.append(Raul)
     NoAtendidos.append(Jose)
     NoAtendidos.append(Sofia)
+    #inserto los pacientes a la lista "NoAtendidos", sin tener en cuenta prioridades
+    #no nos importa el orden de la fila (prioridades) en este test, solo  importa que los atienda desde el principio de la fila (pos[0])
+
     
     Medico1.Atender_Paciente(NoAtendidos)
+    #atiende a Raul
     
     assert NoAtendidos[0].DNI == Jose.DNI
     assert NoAtendidos[0].DNI != Raul.DNI
+    #Raul ya no se encuentra en la fila, ahora el primero es Jose
     
     Medico1.Atender_Paciente(NoAtendidos)
-    
+    #atiende a Jose 
+        
     assert NoAtendidos[0].DNI == Sofia.DNI
     assert NoAtendidos[0].DNI != Jose.DNI
-    
+    #Jose ya no se encuentra en la fila, ahora la primera es Sofia
     
     
