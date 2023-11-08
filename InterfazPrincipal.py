4347d7381bb7ca9afc6da8b4a1b664e4f5587ed9
SFiterman <ing.solfiterman@gmail.com>
2023-11-07 21:34:22 -0300

    cambios :)

--- InterfazPrincipal.py
+++ InterfazPrincipal.py
@@ -35,7 +35,6 @@ canvas_height = 700
 # Asigno colores a pacientes y los muestro en interfaz
 def asignar_colores_pacientes():
     global pacientes
-    # Crear una lista de pacientes
     pacientes = LecturaArchivoSimulacion()
     pacientes = pacientes[:120]
 
@@ -45,55 +44,52 @@ def asignar_colores_pacientes():
         enfermero.Asignar_Lugar_FilaDC(paciente, PacientesOrdenados)
 
     for idx, paciente in enumerate(PacientesOrdenados):
-        # Color del cuadrado
-        color_pac = colores_mapping.get(paciente.ColorP.Color, "white")
+        color_pac = colores_mapping.get(paciente.ColorP.Color, "white") # Color del cuadrado = color del paciente
 
-        # Calcular la fila y la columna en la cuadrícula
+        #Calculo de fila y columna en la cuadrícula
         fila = idx // num_columnas
         columna = idx % num_columnas
 
-        # Calcular las coordenadas para el cuadrado del paciente
+        # Calculo coordenadas cuadrado del paciente
         x1 = columna * (tamano_cuadrado + espacio_entre_pacientes)
         y1 = fila * (tamano_cuadrado + espacio_entre_pacientes)
         x2 = x1 + tamano_cuadrado
         y2 = y1 + tamano_cuadrado
 
-        # Crear un cuadrado con el color de fondo del paciente en el canvas
+        # Creo cuadrado del paciente con su color asignado
         canvas.create_rectangle(x1, y1, x2, y2, fill=color_pac)
 
-        # Etiqueta con el nombre del paciente
+        #Nombre pac
         canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2, text=f"{paciente.Nombre}", fill="black")
 
-# Atender a los pacientes
 def atender_pacientes():
     global pacientes
     for paciente in PacientesOrdenados:
         medico = Medico()
         medico.Atender_Paciente([paciente])
 
-        # Color del cuadrado del paciente
         color_pac = colores_mapping.get(paciente.ColorP.Color, "white")
 
-        # Calcular la fila y la columna en la cuadrícula
+        #Fila y la columna en la cuadrícula
         fila = PacientesOrdenados.index(paciente) // num_columnas
         columna = PacientesOrdenados.index(paciente) % num_columnas
 
-        # Calcular las coordenadas para el cuadrado del paciente
+        # Coordenadas para el cuadrado del paciente
         x1 = columna * (tamano_cuadrado + espacio_entre_pacientes)
         y1 = fila * (tamano_cuadrado + espacio_entre_pacientes)
         x2 = x1 + tamano_cuadrado
         y2 = y1 + tamano_cuadrado
 
-        # Mostrar el estado del paciente (Vivo o Muerto) dentro del cuadrado del paciente
+        # Estado del paciente (Vivo o Muerto) dentro del cuadrado
         estado_paciente = "Vivo" if paciente.Vivo else "Muerto"
         canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2 + 15, text=estado_paciente, fill="black")
 
-# Crear la ventana principal
+#ventana principal
 window = tk.Tk()
 window.title("Hospital FitBares")
 window.geometry("1000x800")
 
-# Cargar y mostrar la imagen en un marco en la parte inferior
+#imagen en la parte inferior
 imagen = Image.open("logo2.gif")
 
 #Cambio el tamaño de la imagen
@@ -108,21 +104,20 @@ imagen_label = tk.Label(imagen_frame, image=imagen)
 imagen_label.pack()
 
 
-# Crear un marco para los botones y configurar la orientación para que estén uno al lado del otro
+#botones, orientación: uno al lado del otro
 button_frame = tk.Frame(window)
 button_frame.pack(side="top")
 
-# Crear un botón para asignar colores a los pacientes
+
 asignar_button = tk.Button(button_frame, text="Asignar Colores", command=asignar_colores_pacientes)
 asignar_button.pack(side="left")
 
-# Crear un botón para atender a los pacientes
 atender_button = tk.Button(button_frame, text="Atender Pacientes", command=atender_pacientes)
 atender_button.pack(side="left")
 
-# Crear un canvas para mostrar a los pacientes
+# Canvas para mostrar a los pacientes
 canvas = tk.Canvas(window, width=canvas_width, height=canvas_height, bg="white")
 canvas.pack()
 
-# Iniciar el bucle principal de la interfaz
+
 window.mainloop()
