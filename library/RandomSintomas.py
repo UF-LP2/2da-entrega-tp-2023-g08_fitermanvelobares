1d1ca594ff35800deafbf5d7e50f40f54aa9d007
SFiterman <ing.solfiterman@gmail.com>
2023-11-21 12:32:40 -0300

    colores :)

--- library/RandomSintomas.py
+++ library/RandomSintomas.py
@@ -8,18 +8,25 @@ def EnvejecerPacientes(ListaPacientes:List[Paciente]):
     SintomasPosibles = ["No Respira", "Politraumatismo Grave", "Coma", "Convulsiones", "Hemorragia Digestiva", 
                         "Isquemia", "Cefalea Brusca", "Paresia", "Hipertension Arterial", 
                         "Vertigo Con Afectacion Vegetativa","Sincope", "Urgencias Psiquiatricas", "Otalgias",
-                        "Odontalgias", "Dolores Inespecíficos Leves", "Traumatismos", "Esguinces", "hipo", 
-                        "resfrio", "me duele el pie", "piojos"]
+                        "Odontalgias", "Dolores Inespecíficos Leves", "Traumatismos", "Esguinces"]
     #^todos los sinotmas que puede tener el paciente
     
     enfermeroAux = Enfermero() #creo un enfermero
     
-    CantidadRdn =  random.randint(0, len(ListaPacientes)-1) #decido cuantos pacientes experimentaron nuevos sintomas (puede que terminen bajando de nivel)
+    CantidadRdn =  random.randint(0, len(ListaPacientes)-1) #decido cuantos pacientes experimentaron nuevos sintomas
     print("Pacientes envecejidos: " +str(CantidadRdn))
     
     for k in range(0,CantidadRdn):
         PosRandLista = random.sample(range(len(ListaPacientes)), 1)[0] #elijo al paciente en lista que le voy a cambiar los sintomas
-        AuxNuevoSintoma = random.randint(0, len(SintomasPosibles)-1) #decido que sintoma nuevo tiene
+        if (ListaPacientes[PosRandLista].ColorP.Color == "Naranja" or ListaPacientes[PosRandLista].ColorP.Color == "Rojo"):    
+            AuxNuevoSintoma = random.randint(0,1) #Los sintomas rojos
+        elif(ListaPacientes[PosRandLista].ColorP.Color == "Amarillo"):
+            AuxNuevoSintoma = random.randint(0,5) #Los sintomas rojos y naranjas
+        elif(ListaPacientes[PosRandLista].ColorP.Color == "Verde"): 
+            AuxNuevoSintoma = random.randint(0,11) #Los sintomas rojos, naranjas y amarillos
+        else: #es azul
+            AuxNuevoSintoma = random.randint(0, len(SintomasPosibles)-1) #decido que sintoma nuevo tiene
+            
         ListaPacientes[PosRandLista].Sintomas.clear #elimino los sintomas viejos del paciente
         ListaPacientes[PosRandLista].Sintomas[0] = SintomasPosibles[AuxNuevoSintoma] #le asigno el nuevo sintoma
         ListaPacientes[PosRandLista].Envejecido = True #lo marco como envejecido, asi mantiene el horario de triage anterior
